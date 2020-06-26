e3253181221dfb4e2c843d069f23381d91bdc26e
santomat-5111 <60806314+santomat-5111@users.noreply.github.com>
2020-06-26 11:16:11 +0530

    Linking phaser
    Linked phaser to index.html and also added a game initialising window with the help of JavaScript

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(__dirname + '/public/'));
+
+app.get('/', (req, res) => res.sendFile('index.html'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
---
+++ public/js/singlephase.js
@@ -0,0 +1,9 @@
+const config = {
+    width: 640,
+    height: 640,
+    type: Phaser.AUTO,
+    parent: 'phaser-game',
+    scene: []
+};
+
+new Phaser.Game(config);

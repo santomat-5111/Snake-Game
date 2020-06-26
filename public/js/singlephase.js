f5101efaeb0e8d29a0cb1f9e94603beff5d40fd5
santomat-5111 <60806314+santomat-5111@users.noreply.github.com>
2020-06-26 13:23:24 +0530

    Created the Main Scene for the game

--- public/js/singlephase.js
+++ public/js/singlephase.js
@@ -1,9 +1,10 @@
+import MainScene from './MainScene.js'
 const config = {
     width: 640,
     height: 640,
     type: Phaser.AUTO,
     parent: 'phaser-game',
-    scene: []
+    scene: [MainScene]
 };
 
 new Phaser.Game(config);

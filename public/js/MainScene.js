e2ff24cdbb55c37d214cad7ba1e0cef11765c79a
santomat-5111 <60806314+santomat-5111@users.noreply.github.com>
2020-06-27 16:31:54 +0530

    Final Commit

--- public/js/MainScene.js
+++ public/js/MainScene.js
@@ -1,15 +1,15 @@
-import Snake from './Snake.js'
+import Snake from './Snake.js';
 
 export default class MainScene extends Phaser.Scene {
-    constructor() {
-        super('MainScene');
-    }
+  constructor() {
+    super('MainScene');
+  }
 
-    create(){
-        this.snake = new Snake(this);
-    }
+  create() {
+    this.snake = new Snake(this);
+  }
 
-    update(time){
-        this.snake.update(time);
-    }
+  update(time, delta) {
+    this.snake.update(time);
+  }
 }
--- public/js/singlephase.js
+++ public/js/singlephase.js
@@ -1,10 +1,11 @@
-import MainScene from './MainScene.js'
+import MainScene from './MainScene.js';
+
 const config = {
-    width: 640,
-    height: 640,
-    type: Phaser.AUTO,
-    parent: 'phaser-game',
-    scene: [MainScene]
+  width: 640,
+  height: 640,
+  type: Phaser.AUTO,
+  parent: 'phaser-game',
+  scene: [MainScene]
 };
 
 new Phaser.Game(config);
